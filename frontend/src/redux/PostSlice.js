2b9f5673a023089c98976f404765ab43e8bc821d
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-05-15 15:26:41 +0530

    implemented redux in frontend

---
+++ frontend/src/redux/PostSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const PostSlice = createSlice({
+  name: "post",
+  initialState: {
+    posts: [
+      {
+        posttitle: "",
+        postcontent: "",
+        creationdate: "",
+        like: "",
+        comments: [
+          {
+            comment: "",
+            username: "",
+          },
+        ],
+      },
+    ],
+  },
+  reducers: {
+    // SetUsername: (state, action) => {
+    //   state.value += action.payload;
+    // },
+    // SetEmail: (state, action) => {
+    //   state.value += action.payload;
+    // },
+  },
+});
+
+export const { SetUsername, SetEmail } = PostSlice.actions;
+
+export default PostSlice.reducer;
---
+++ frontend/src/redux/UserSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const UserSlice = createSlice({
+  name: "user",
+  initialState: {
+    username: "",
+    email: "",
+    authenticated: false,
+    posts: [
+      {
+        posttitle: "",
+        postcontent: "",
+        creationdate: "",
+        like: "",
+        comments: [
+          {
+            comment: "",
+            username: "",
+          },
+        ],
+      },
+    ],
+  },
+  reducers: {
+    SetUsername: (state, action) => {
+      state.value += action.payload;
+    },
+    SetEmail: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { SetUsername, SetEmail } = UserSlice.actions;
+
+export default UserSlice.reducer;
