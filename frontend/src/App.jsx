b45a8b78edefe43f4af735814c364b625aead282
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-06-14 06:45:00 +0530

    added packages in frontend and backend, i have removed the previous backend of the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,8 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Layouts from "./layouts/Layouts";
-import IndexPage from "./pages/IndexPage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layouts />}>
-          <Route index element={<IndexPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Route>
-      </Routes>
+    <h1 className="d-flex justify-content-center">Application is working</h1>
     </>
   );
 }
